69f643fcea232b598175d682da14d6bd6781f81c
jesus uriel perez <jeslet.fres@gmail.com>
2019-12-03 01:02:21 -0600

    Agregando editar

--- src/main/java/com/demo/springboot/app/controller/ClienteController.java
+++ src/main/java/com/demo/springboot/app/controller/ClienteController.java
@@ -1,18 +1,24 @@
 package com.demo.springboot.app.controller;
 
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.demo.springboot.app.models.dao.IClienteDao;
 import com.demo.springboot.app.models.entity.Cliente;
 
 @Controller
+@SessionAttributes("cliente")
 public class ClienteController {
 	
 	@Autowired
@@ -34,8 +40,28 @@ public class ClienteController {
 		
 	}
 	
+	@RequestMapping(value="/form/{id}")
+	public String editar(@PathVariable (value="id") Long id, Map<String, Object> model) {
+		
+		Cliente cliente = null;
+		
+		if(id>0) {
+			
+			cliente = clienteDao.findOne(id); 	
+			
+		}else {
+			
+			return "redirect:/listar";
+			
+		}
+		model.put("cliente", cliente);
+		model.put("titulo", "Editar cliente");
+		return "form";
+		
+	}
+	
 	@RequestMapping(value="/form", method = RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
 		if(result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario de cliente");
 			return "form";
@@ -43,6 +69,7 @@ public class ClienteController {
 		}
 		
 		clienteDao.save(cliente);
+		status.setComplete();
 		return "redirect:listar";
 	}
 
